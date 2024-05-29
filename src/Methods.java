25e3cf7b4dc76dd43042a4539752b1694ce09fcf
Fel-ly <kiokofpm@gmail.com>
2024-05-29 22:32:27 +0300

    methods

---
+++ src/Methods.java
@@ -0,0 +1,14 @@
+package src;
+
+public class Methods {
+
+    public static void main(String[] args) {
+        String name = "Raha";
+
+        greetings(name); //call method 'greetings'
+    }
+
+    static void greetings(String namee) {
+        System.out.println("Heey "+ namee);
+    }
+}
